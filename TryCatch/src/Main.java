b9643e86f32c559f0c2f79c99108e7984f73d224
Sri Danthuluru <Sri.Danthuluru@godigit.com>
2024-08-07 17:35:59 +0530

    day 8

---
+++ TryCatch/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=12;
+        int b=0;
+        try{
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Divided by zero Exception");
+        }
+
+
+    }
+}
